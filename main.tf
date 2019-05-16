13f4c1eaaf799915b0c23a98eec5aca3594ee9bd
Joel Cripps <jcripps@fortinet.com>
2019-05-16 12:53:46 -0700

    Fixed SLB now uses tcp instead of http Added second vswitch and changed
    zone selection from pirorty to balanced
    
    Change-Id: Ieb4465aadbbff740dcc4a6325fd1d4be198c5c5b

--- main.tf
+++ main.tf
@@ -33,9 +33,9 @@ variable "cluster_name"{
 data "alicloud_instance_types" "types_ds" {
   cpu_core_count = 2
   memory_size = 8
-  instance_type_family = "ecs.g5"
+  instance_type_family = "ecs.g5"//"ecs.sn2ne"
 }
-//SLB
+
 data "alicloud_regions" "current_region_ds" {
   current = true
 }
@@ -44,18 +44,28 @@ data "alicloud_zones" "default" {}
 resource "random_string" "psk" {
   length = 16
   special = true
-  override_special = "/@\" "
+  override_special = ""
+
+}
+//Random 3 char string appended to the ened of each name to avoid conflicts
+//If you increase this number you will need to adjust OTS name since it will exceed the max 16 chars.
+resource "random_string" "random_name_post" {
+  length = 3
+  special = true
+  override_special = ""
+  min_lower = 3
 }
 resource "alicloud_ram_role" "ram_role" {
-  name = "fortigateautoscaleLogging"
-  services = ["apigateway.aliyuncs.com"]
-  description = "this is a role test."
+  name = "${var.cluster_name}-Logging-Role-${random_string.random_name_post.result}"
+  services = ["fc.aliyuncs.com"]
+  description = "AutoScale Logging Role, created by Terraform"
   force = true
+   depends_on = ["alicloud_ram_policy.policy"]
 }
 
 resource "alicloud_ram_policy" "policy" {
-  name = "policyName"
-  depends_on = ["alicloud_ram_role.ram_role","alicloud_log_project.AutoScaleLogging", "alicloud_log_store.AutoScaleLogging-Store"]
+  name = "${var.cluster_name}-Logging-Policy-${random_string.random_name_post.result}"
+  depends_on = ["alicloud_log_project.AutoScaleLogging", "alicloud_log_store.AutoScaleLogging-Store"]
   statement = [
     {
       action = [
@@ -68,10 +78,15 @@ resource "alicloud_ram_policy" "policy" {
   description = "FortiGate AutoScale Logging Policy"
   force = true
 }
+resource "alicloud_ram_role_policy_attachment" "attach"{
+  policy_name = "${alicloud_ram_policy.policy.name}"
+  policy_type = "${alicloud_ram_policy.policy.type}"
+  role_name = "${alicloud_ram_role.ram_role.name}"
+}
 
 resource "alicloud_vpc" "vpc" {
   cidr_block = "172.16.0.0/16"
-  name = "${var.cluster_name}"
+  name = "${var.cluster_name}-${random_string.random_name_post.result}"
 }
 
 
@@ -80,10 +95,16 @@ resource "alicloud_vswitch" "vsw" {
   cidr_block        = "172.16.0.0/21"
   availability_zone = "${data.alicloud_zones.default.zones.0.id}"
 }
+//zone B
+resource "alicloud_vswitch" "vsw2" {
+  vpc_id            = "${alicloud_vpc.vpc.id}"
+  cidr_block        = "172.16.8.0/21"
+  availability_zone = "${data.alicloud_zones.default.zones.1.id}"
+}
 
 
 resource "alicloud_slb" "default" {
-  name                 = "${var.cluster_name}.SLB"
+  name                 = "${var.cluster_name}.SLB-${random_string.random_name_post.result}"
   internet             = true
   internet_charge_type = "PayByTraffic"
   bandwidth            = 5
@@ -92,7 +113,7 @@ resource "alicloud_slb" "default" {
   slave_zone_id        = "${data.alicloud_zones.default.zones.1.id}"//second available zone.
 }
 resource "alicloud_slb_acl" "acl" {
-  name = "autoscaleSLB_ACL"
+  name = "${var.cluster_name}-ACL-${random_string.random_name_post.result}"
   ip_version = "ipv4"
   entry_list = [
     {
@@ -111,16 +132,17 @@ resource "alicloud_slb_acl" "acl" {
 }
 resource "alicloud_slb_listener" "http" {
   load_balancer_id = "${alicloud_slb.default.id}"
-  backend_port = 80
-  frontend_port = 80
+  backend_port = 443
+  frontend_port = 443
   health_check = "on"
-  health_check_connect_port = 80
-  bandwidth = 10
-  protocol = "http"
+  health_check_connect_port = 443
+  bandwidth = 100
+  protocol = "tcp"
   sticky_session = "on" //Persistent session
-  sticky_session_type = "insert"
+  sticky_session_type = "server" //Fortigate Serves the cookie.
   cookie = "FortiGateAutoScaleSLB"
   cookie_timeout = 86400
+  persistence_timeout = 3600
   acl_status                = "off"
   acl_type                  = "white"
   acl_id                    = "${alicloud_slb_acl.acl.id}"
@@ -128,7 +150,7 @@ resource "alicloud_slb_listener" "http" {
 
 //Security Group
 resource "alicloud_security_group" "SecGroup" {
-  name        = "FortigateAutoscale_SecGroup"
+  name        = "${var.cluster_name}-SecGroup-${random_string.random_name_post.result}"
   description = "New security group"
   vpc_id = "${alicloud_vpc.vpc.id}"
 }
@@ -162,11 +184,12 @@ resource "alicloud_ess_scaling_group" "scalinggroups_ds" {
     
   // Autoscaling Group
   depends_on = ["alicloud_slb_listener.http"]
-  scaling_group_name = "FortigateAutoScale"
+  scaling_group_name = "${var.cluster_name}-${random_string.random_name_post.result}"
   min_size           = 2
   max_size           = 3
   removal_policies   = ["OldestInstance", "NewestInstance"]
-  vswitch_ids = ["${alicloud_vswitch.vsw.id}"]
+  vswitch_ids = ["${alicloud_vswitch.vsw.id}", "${alicloud_vswitch.vsw2.id}"]
+  multi_az_policy = "BALANCE"
 
 
   loadbalancer_ids = [
@@ -180,30 +203,152 @@ resource "alicloud_ess_scaling_configuration" "config" {
   force_delete = true
   scaling_group_id  = "${alicloud_ess_scaling_group.scalinggroups_ds.id}"
   image_id          = "${data.alicloud_images.ecs_image.images.0.id}" //grab the first image that matches the regex
-  instance_type     = "${data.alicloud_instance_types.types_ds.instance_types.0.id}"//Grab the first instance that meets the requirements. Default 2 Cpu 8GB memory. //ecs.g5.xlarge" 
+  instance_type     = "${data.alicloud_instance_types.types_ds.instance_types.0.id}"//Grab the first instance that meets the requirements. Default 2 Cpu 8GB memory.
   security_group_id = "${alicloud_security_group.SecGroup.id}"
   internet_charge_type = "PayByTraffic"
   active = true
   enable = true
   user_data = "{'config-url':'https://${data.alicloud_account.current.id}.${var.region}-internal.fc.aliyuncs.com/2016-08-15/proxy/${alicloud_fc_service.fortigate-autoscale-service.name}/${alicloud_fc_function.fortigate-autoscale.name}/'}"
   depends_on = ["alicloud_fc_service.fortigate-autoscale-service","alicloud_fc_function.fortigate-autoscale","alicloud_fc_trigger.httptrigger","alicloud_oss_bucket_object.object-content","alicloud_ots_instance.tablestore"]
-  
+  internet_max_bandwidth_in = 200
+  internet_max_bandwidth_out = 100
 }
 
+//Scaling Rule
+//Scale Out
+resource "alicloud_ess_scaling_rule" "scale_out" {
+  scaling_group_id = "${alicloud_ess_scaling_group.scalinggroups_ds.id}"
+  scaling_rule_name = "ScaleIn"
+  adjustment_type  = "QuantityChangeInCapacity"
+  adjustment_value = 1
+  cooldown         = 60
+}
+//Scale In
+resource "alicloud_ess_scaling_rule" "scale_in" {
+  scaling_group_id = "${alicloud_ess_scaling_group.scalinggroups_ds.id}"
+  scaling_rule_name = "ScaleOut"
+  adjustment_type  = "QuantityChangeInCapacity"
+  adjustment_value = -1
+  cooldown         = 60
+}
+//Scaling Alarm
+//Scale Out 
+resource "alicloud_ess_alarm" "cpu_alarm_scale_out" {
+    name = "Fortigate_cpu_alarm_scale_out__${random_string.random_name_post.result}"
+    description = "FortiGate AutoScaleCPU utilization alert"
+    //Ari is the unique identifier for a scaling rule
+    alarm_actions = ["${alicloud_ess_scaling_rule.scale_out.ari}"]
+    scaling_group_id = "${alicloud_ess_scaling_group.scalinggroups_ds.id}"
+    metric_type = "system"
+    metric_name = "CpuUtilization"
+    //Average over 300 seconds - only supports 60/120/300/900
+    period = 300
+    statistics = "Average"
+    threshold = 60
+    comparison_operator = ">="
+    evaluation_count = 3 
+}
 
+//Scale In
+resource "alicloud_ess_alarm" "cpu_alarm_scale_in" {
+    name = "Fortigate-cpu_alarm_scale_in_${random_string.random_name_post.result}"
+    description = "FortiGate AutoScaleCPU utilization alert"
+    //Ari is the unique identifier for a scaling rule
+    alarm_actions = ["${alicloud_ess_scaling_rule.scale_in.ari}"]
+    scaling_group_id = "${alicloud_ess_scaling_group.scalinggroups_ds.id}"
+    metric_type = "system"
+    metric_name = "CpuUtilization"
+    //Average over 900 seconds - only supports 60/120/300/900
+    period = 900
+    statistics = "Average"
+    threshold = 35
+    comparison_operator = "<="
+    evaluation_count = 3 
+}
 
-
-
-# Create an OTS instance
+// Create an OTS instance
 resource "alicloud_ots_instance" "tablestore" {
-  name = "FortiGateASG2" //16 char limit
+  name = "FortiGateASG-${random_string.random_name_post.result}" //16 char limit
   description = "TableStore Instance Terraform"
   accessed_by = "Any"
+  instance_type = "HighPerformance"
   tags {
     Created = "TF"
     For = "FortiGate AutoScale Table"
   }
 }
+//Create the Tables
+//While not neccessary, we create them here to allow terraform to destroy/manage them
+//If they are not created during the terraform apply, they will be created by the function
+resource "alicloud_ots_table" "table_FortiAnalyzer" {
+  instance_name = "${alicloud_ots_instance.tablestore.name}"
+  table_name = "FortiAnalyzer"
+  primary_key = [
+    {
+      name = "instanceId"
+      type = "String"
+    }
+  ]
+  time_to_live = "-1"
+  max_version = "1"
+
+}
+resource "alicloud_ots_table" "table_FortiGateLifecycleItem" {
+  instance_name = "${alicloud_ots_instance.tablestore.name}"
+  table_name = "FortiGateLifecycleItem"
+  primary_key = [
+    {
+      name = "instanceId"
+      type = "String"
+    }
+  ]
+  time_to_live = "-1"
+  max_version = "1"
+
+}
+resource "alicloud_ots_table" "table_FortiGateMasterElection" {
+  instance_name = "${alicloud_ots_instance.tablestore.name}"
+  table_name = "FortiGateMasterElection"
+  primary_key = [
+    {
+      name = "asgName"
+      type = "String"
+    }
+  ]
+  time_to_live = "-1"
+  max_version = "1"
+
+}
+resource "alicloud_ots_table" "table_Settings" {
+  instance_name = "${alicloud_ots_instance.tablestore.name}"
+  table_name = "Settings"
+  primary_key = [
+    {
+      name = "settingKey"
+      type = "String"
+    }
+  ]
+  time_to_live = "-1"
+  max_version = "1"
+
+}
+resource "alicloud_ots_table" "table_FortiGateAutoscale" {
+  instance_name = "${alicloud_ots_instance.tablestore.name}"
+  table_name = "FortiGateAutoscale"
+  primary_key = [
+    {
+      name = "instanceId"
+      type = "String"
+    }
+  ]
+  time_to_live = "-1"
+  max_version = "1"
+
+}
+// OTS table. If not created in Terraform they will create as part of the AutoScale Function code.
+// In this case they will not be managed by Terraform and a destroy will not work
+
+
 //OSS 
  resource "local_file" "LocalConfigWrite" {
     content     = <<EOF
@@ -215,7 +360,7 @@ config system auto-scale
    set status enable
    set sync-interface {SYNC_INTERFACE}
    set role master
-   set callback-url https://${data.alicloud_account.current.id}.${var.region}.fc.aliyuncs.com/2016-08-15/proxy/${alicloud_fc_service.fortigate-autoscale-service.name}/${alicloud_fc_function.fortigate-autoscale.name}/
+   set callback-url https://${data.alicloud_account.current.id}.${var.region}-internal.fc.aliyuncs.com/2016-08-15/proxy/${alicloud_fc_service.fortigate-autoscale-service.name}/${alicloud_fc_function.fortigate-autoscale.name}/
    set psksecret {PSK_SECRET}
 
 end
@@ -224,7 +369,7 @@ end
     filename = "./cloud-init.sh"
 }
 resource "alicloud_oss_bucket" "FortiGateAutoScaleConfig" {
-  bucket = "fortigateautoscaleconfig" //Must be in lower case.
+  bucket = "fortigateautoscaleconfig-${random_string.random_name_post.result}" //Must be in lower case.
   acl = "public-read"
 }
 
@@ -237,7 +382,8 @@ resource "alicloud_oss_bucket_object" "object-content" {
 
 //Create the Function Service
 resource "alicloud_fc_service" "fortigate-autoscale-service" {
-    name = "fortigate-autoscale-service"
+    depends_on = ["alicloud_ram_role.ram_role"]
+    name = "${var.cluster_name}-${random_string.random_name_post.result}" //Removed "service" from name to keep URL under 127 characters.
     description = "Created by terraform"
     internet_access = true
     role = "${alicloud_ram_role.ram_role.arn}"
@@ -252,19 +398,19 @@ resource "alicloud_fc_service" "fortigate-autoscale-service" {
 //Function
 resource "alicloud_fc_function" "fortigate-autoscale" {
   service = "${alicloud_fc_service.fortigate-autoscale-service.name}"
-  name = "fortigate-autoscale"
+  name = "${var.cluster_name}-${random_string.random_name_post.result}"
   description = "FortiGate AutoScale - AliCloud Created by Terraform"
-  filename = "./AutoScaleAliCloudTerraformBuild.zip"
+  filename = "./FortiGateAutoScaleFunctionCode.zip"
   memory_size = "512"
   runtime = "nodejs8"
   handler = "index.handler",
-  timeout = "200",
+  timeout = "500",
   environment_variables {
     managedby = "Created with Terraform"
     FORTIGATE_PSKSECRET = "${random_string.psk.result}"
     REGION_ID = "${var.region}"
     ENDPOINT_ESS = "https://ess.aliyuncs.com"
-    ENDPOINT_ECS = "https://ecs.aliyuncs.com"
+    ENDPOINT_ECS = "https://ecs.${var.region}.aliyuncs.com"
     ACCESS_KEY_SECRET = "${var.secret_key}"
     ACCESS_KEY_ID = "${var.access_key}"
     OSS_ENDPOINT = "oss-${var.region}.aliyuncs.com"
@@ -272,10 +418,10 @@ resource "alicloud_fc_function" "fortigate-autoscale" {
     REGION_ID_OSS = "oss-${var.region}"
     CLIENT_TIMEOUT = 3000 //default
     DEFAULT_HEART_BEAT_INTERVAL = 10000
-    HEART_BEAT_DELAY_ALLOWANCE = 3000
-    SCRIPT_EXECUTION_EXPIRE_TIME = "180000 + Date.now()"
-    SCRIPT_EXECUTION_TIME_CHECKPOINT = 3000
-    SCRIPT_TIMEOUT = 200
+    HEART_BEAT_DELAY_ALLOWANCE = 25000
+    SCRIPT_EXECUTION_EXPIRE_TIME = 350000
+    SCRIPT_EXECUTION_TIME_CHECKPOINT = 5000
+    SCRIPT_TIMEOUT = 500
     TABLE_STORE_END_POINT ="https://${alicloud_ots_instance.tablestore.name}.${var.region}.ots.aliyuncs.com"
     TABLE_STORE_INSTANCENAME ="${alicloud_ots_instance.tablestore.name}"
     AUTO_SCALING_GROUP_NAME="${alicloud_ess_scaling_group.scalinggroups_ds.scaling_group_name}"
@@ -319,20 +465,33 @@ resource "alicloud_fc_trigger" "httptrigger" {
 
 
 resource "alicloud_log_project" "AutoScaleLogging" {
-  name        = "fortigateautoscalelog" //Name must be lower case
+  name        = "fortigateautoscalelog-${random_string.random_name_post.result}" //Name must be lower case
   description = "created by terraform"
 }
 
 resource "alicloud_log_store" "AutoScaleLogging-Store" {
   project               = "${alicloud_log_project.AutoScaleLogging.name}"
-  name                  = "autoscalelog-store"
+  name                  = "autoscalelog-store-${random_string.random_name_post.result}"
   shard_count           = 3
   auto_split            = true
   max_split_shard_count = 60
   append_meta           = true
   retention_period = 15
 }
-
+resource "alicloud_log_store_index" "log_store_index" {
+  project = "${alicloud_log_project.AutoScaleLogging.name}"
+  logstore = "${alicloud_log_store.AutoScaleLogging-Store.name}"
+  full_text {
+    case_sensitive = true
+    token = " #$%^*\r\n\t"
+  }
+  field_search = [
+    {
+      name = "${alicloud_fc_function.fortigate-autoscale.name}"
+      enable_analytics = true
+    }
+  ]
+}
 output "triggerUrl"{
     value = "${alicloud_ess_scaling_configuration.config.user_data}"
 
