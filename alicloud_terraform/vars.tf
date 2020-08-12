2a78ecbca95643fa247e7553da13beab1d9c5d8b
Joel Cripps <jcripps@fortinet.com>
2020-08-11 17:17:29 -0700

    lock provider version

--- alicloud_terraform/vars.tf
+++ alicloud_terraform/vars.tf
@@ -14,7 +14,7 @@ provider "alicloud" {
     access_key = "${var.access_key}"
     secret_key = "${var.secret_key}"
     region     = "${var.region}"
-     version    = "=1.48.0"
+    version    = "=1.48.0"
 }
 variable "region" {
     type = "string"
